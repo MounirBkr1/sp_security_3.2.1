41d0ff6c89470496c9b3a566da559b616e3828ec
Mounir Bkr <miirbri1@gmail.com>
2024-07-10 08:56:38 +0100

    initial commit

---
+++ src/main/java/com/mnr/sp_security3/SpSecurity3Application.java
@@ -0,0 +1,13 @@
+package com.mnr.sp_security3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSecurity3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpSecurity3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mnr/sp_security3/SpSecurity3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.sp_security3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSecurity3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
